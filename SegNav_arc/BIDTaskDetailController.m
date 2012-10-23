26d19044d58bc3cb57476704698f2bf691888eb6
closer27 <closer527@gmail.com>
2012-10-23 22:04:10 +0900

    story board test

---
+++ SegNav_arc/BIDTaskDetailController.m
@@ -0,0 +1,38 @@
+//
+//  BIDTaskDetailController.m
+//  SegNav_arc
+//
+//  Created by 승원 김 on 12. 10. 23..
+//  Copyright (c) 2012년 승원 김. All rights reserved.
+//
+
+#import "BIDTaskDetailController.h"
+
+@interface BIDTaskDetailController ()
+
+@end
+
+@implementation BIDTaskDetailController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SegNav_arc/BIDTaskListController.h
@@ -0,0 +1,13 @@
+//
+//  BIDTaskListController.h
+//  SegNav_arc
+//
+//  Created by 승원 김 on 12. 10. 23..
+//  Copyright (c) 2012년 승원 김. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDTaskListController : UITableViewController
+
+@end
