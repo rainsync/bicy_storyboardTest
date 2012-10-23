7cc3292d7e91fa7869b622fa095b97e1b3d09984
closer27 <closer527@gmail.com>
2012-10-23 21:14:35 +0900

    Initial Commit

---
+++ SegNav_arc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SegNav_arc
+//
+//  Created by 승원 김 on 12. 10. 23..
+//  Copyright (c) 2012년 승원 김. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
